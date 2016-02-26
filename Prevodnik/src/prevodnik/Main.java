c7217eb0fb0b318bcc5e9516d8f347ce1f155f2a
buzzcz <buzz3333@gmail.com>
2016-02-26 11:12:19 +0100

    Changed window size. Changed setting fonts

--- Prevodnik/src/prevodnik/Main.java
+++ Prevodnik/src/prevodnik/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Prevodnik.fxml"));
         primaryStage.setTitle("PřeVODNÍK");
-        primaryStage.setScene(new Scene(root, 630, 382));
+        primaryStage.setScene(new Scene(root, 630, 550));
         primaryStage.show();
     }
 
