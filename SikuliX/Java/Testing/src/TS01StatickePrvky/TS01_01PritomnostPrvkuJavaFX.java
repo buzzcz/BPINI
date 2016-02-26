1683b47452c75146282d0f2e66de4e29b6cb488c
buzzcz <buzz3333@gmail.com>
2016-02-26 11:39:55 +0100

    Added pngs Cleared JavaFX  test suite Added TC_01_01_01 and TC01_01_02 to web suite

--- SikuliX/Java/Testing/src/TS01StatickePrvky/TS01_01PritomnostPrvkuJavaFX.java
+++ SikuliX/Java/Testing/src/TS01StatickePrvky/TS01_01PritomnostPrvkuJavaFX.java
@@ -5,16 +5,17 @@ import org.apache.logging.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.sikuli.basics.Debug;
 import org.sikuli.basics.Settings;
-import org.sikuli.script.App;
-import org.sikuli.script.Key;
-import org.sikuli.script.Pattern;
-import org.sikuli.script.Screen;
+import org.sikuli.script.*;
 
 import javax.swing.*;
 import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * @author Jaroslav Klaus
@@ -32,7 +33,7 @@ public class TS01_01PritomnostPrvkuJavaFX {
 	}
 
 
-	private String screenshotName() {
+	private static String screenshotName() {
 		LocalDateTime l = LocalDateTime.now();
 		return l.getYear() + "" + l.getMonthValue() + "" + l.getDayOfMonth() + "" + l.getHour() + "" + (l.getMinute()
 				< 10 ? "0" + l.getMinute() : l.getMinute()) + "" + l.getSecond() + "";
@@ -44,35 +45,12 @@ public class TS01_01PritomnostPrvkuJavaFX {
 
 		Settings.OcrTextSearch = true;
 		Settings.OcrTextRead = true;
+		Settings.MoveMouseDelay = 0;
 		Debug.setLogger(logger);
 		Debug.setLoggerAll("info");
 
 		collector = new ErrorCollector();
 		s = new Screen();
-		new App("google-chrome").open();
-		chrome = new App("Chrome");
-		chrome.focus();
-		run = true;
-	}
 
-	@AfterClass
-	public static void tearDownAfterClass() {
-		JOptionPane.showMessageDialog(null, "Script dokončen");
-	}
-
-	@Before
-	public void setUp() {
-		try {
-			s.wait("png/addressBar.png", 10);
-			s.click(new Pattern("png/addressBar.png").targetOffset(100, 0));
-			s.paste("http://oks.kiv.zcu.cz/Prevodnik");
-			s.type(Key.ENTER);
-			s.wait("png/zalozkaPrevodnik.png", 5);
-		} catch (Exception e) {
-			run = false;
-
-			s.capture().save("errors", screenshotName());
-			logger.error(e.getMessage());
-		}
 	}
 }
--- SikuliX/Java/Testing/src/TS01StatickePrvky/TS01_01PritomnostPrvkuWeb.java
+++ SikuliX/Java/Testing/src/TS01StatickePrvky/TS01_01PritomnostPrvkuWeb.java
@@ -3,15 +3,19 @@ package TS01StatickePrvky;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.sikuli.basics.Debug;
 import org.sikuli.basics.Settings;
-import org.sikuli.script.App;
-import org.sikuli.script.Screen;
+import org.sikuli.script.*;
 
 import javax.swing.*;
 import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * @author Jaroslav Klaus
@@ -29,7 +33,7 @@ public class TS01_01PritomnostPrvkuWeb {
 	}
 
 
-	private String screenshotName() {
+	private static String screenshotName() {
 		LocalDateTime l = LocalDateTime.now();
 		return l.getYear() + "" + l.getMonthValue() + "" + l.getDayOfMonth() + "" + l.getHour() + "" + (l.getMinute()
 				< 10 ? "0" + l.getMinute() : l.getMinute()) + "" + l.getSecond() + "";
@@ -41,19 +45,78 @@ public class TS01_01PritomnostPrvkuWeb {
 
 		Settings.OcrTextSearch = true;
 		Settings.OcrTextRead = true;
+		Settings.MoveMouseDelay = 0;
 		Debug.setLogger(logger);
 		Debug.setLoggerAll("info");
 
 		collector = new ErrorCollector();
 		s = new Screen();
-		new App("google-chrome").open();
-		chrome = new App("Chrome");
-		chrome.focus();
-		run = true;
+
+		try {
+			new App("google-chrome").open();
+			chrome = new App("Chrome");
+			chrome.focus();
+			run = true;
+			s.wait("png/web/addressBar.png", 10);
+			s.click(new Pattern("png/web/addressBar.png").targetOffset(100, 0));
+			s.paste("http://oks.kiv.zcu.cz/Prevodnik");
+			s.type(Key.ENTER);
+			s.wait("png/web/zalozkaPrevodnik.png", 5);
+		} catch (Exception e) {
+			run = false;
+			s.capture().save("errors", screenshotName());
+			logger.error(e.getMessage());
+		}
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() {
-		JOptionPane.showMessageDialog(null, "Script dokončen");
+		JOptionPane.showMessageDialog(null, "Test Suite dokončen");
+	}
+
+	@Before
+	public void setUp() {
+		try {
+			s.click("png/web/zalozkaPrevodnik.png");
+			s.wait("png/web/tlacitkoPreved.png", 5);
+		} catch (Exception e) {
+			run = false;
+			s.capture().save("errors", screenshotName());
+			logger.error(e.getMessage());
+		}
+	}
+
+	@Test
+	public void TC01_01_01LabelVstup() {
+		if (run) {
+			try {
+				s.find("png/web/vstupLabel.png");
+			} catch (FindFailed | AssertionError e) {
+				s.capture().save("errors", screenshotName());
+				logger.error(e.getMessage());
+				fail(e.getMessage());
+			}
+		} else {
+			run = true;
+			logger.error("Setup failed");
+			fail("Setup failed");
+		}
+	}
+
+	@Test
+	public void TC01_01_02VstupniTextovePole() {
+		if (run) {
+			try {
+				s.find("png/web/vystupLabel.png");
+			} catch (FindFailed | AssertionError e) {
+				s.capture().save("errors", screenshotName());
+				logger.error(e.getMessage());
+				fail(e.getMessage());
+			}
+		} else {
+			run = true;
+			logger.error("Setup failed");
+			fail("Setup failed");
+		}
 	}
 }
